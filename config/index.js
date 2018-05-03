ac7ca32433a5883842bea2ca7552a1a7e29bfdf0
BetsyTeng <287817794@qq.com>
2018-05-03 18:02:44 +0800

    begin

---
+++ config/index.js
@@ -0,0 +1,10 @@
+import path from 'path';
+const config = {
+    server:{
+        port:process.env.PORT || 3001,
+        host:'localhost'
+    },
+    api:{
+        port:process.env.API_PORT || 8088,
+    }
+}

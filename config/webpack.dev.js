4f084b466cf48c4d237b8bcdedddc0da2f365afc
BetsyTeng <31032741+BetsyTeng@users.noreply.github.com>
2019-09-19 14:11:48 +0800

    update

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,52 +1,53 @@
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = "development";
 
-const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 const config = require("./index");
-const baseConfig = require('./webpack.base');
-const path = require('path');
+const baseConfig = require("./webpack.base");
+const path = require("path");
 // require('babel-register');
 
-const {host,port} = config.server;
+const { host, port } = config.server;
 const webpackConfigDev = {
-    ...baseConfig,
-    mode: 'development',
-    devtool: 'cheap-module-eval-source-map',
-    entry:[
-        `webpack-hot-middleware/client?path=http://${host}:${port}`,
-        `${config.client}/index.js`,
-    ],
-    output:{
-        ...baseConfig.output,
-    },
-    devServer: {
-        contentBase: config.client,
-        historyApiFallback: true,
-        hot: false,//关闭热点
-        inline: true,//开启页面刷新
-        host: host,
-        port: port,
-    },
-    plugins: [
-        // “If you are using the CLI, the webpack process will not exit with an error code by enabling this plugin.”
-        // https://github.com/webpack/docs/wiki/list-of-plugins#noerrorsplugin
-        new webpack.NoEmitOnErrorsPlugin(),
-        // https://webpack.github.io/docs/hot-module-replacement-with-webpack.html
-        new webpack.HotModuleReplacementPlugin(),
-        new HTMLWebpackPlugin({
-            title: 'index',
-            template:`${config.client}/index.ejs`,
-            filename:`${config.dist}/index.html`,
-            // inject: 'body',
-            // chunks:['main','vendor'],
-            chunksSortMode: 'dependency',
-        }),
-        // // NODE_ENV should be production so that modules do not perform certain development checks
-        new webpack.DefinePlugin({//定义环境变量为开发环境
-            'process.env.NODE_ENV': JSON.stringify('development')
-        }),
-    ],
-    // https://github.com/chentsulin/webpack-target-electron-renderer#how-this-module-works
-}
+  ...baseConfig,
+  mode: "development",
+  devtool: "cheap-module-eval-source-map",
+  entry: [
+    `webpack-hot-middleware/client?path=http://${host}:${port}`,
+    `${config.client}/index.js`
+  ],
+  output: {
+    ...baseConfig.output
+  },
+  devServer: {
+    contentBase: config.client,
+    historyApiFallback: true,
+    hot: false, //关闭热点
+    inline: true, //开启页面刷新
+    host: host,
+    port: port
+  },
+  plugins: [
+    // “If you are using the CLI, the webpack process will not exit with an error code by enabling this plugin.”
+    // https://github.com/webpack/docs/wiki/list-of-plugins#noerrorsplugin
+    new webpack.NoEmitOnErrorsPlugin(),
+    // https://webpack.github.io/docs/hot-module-replacement-with-webpack.html
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      title: "index",
+      template: `${config.client}/index.html`,
+      filename: `${config.dist}/index.html`,
+      // inject: 'body',
+      // chunks:['main','vendor'],
+      chunksSortMode: "dependency"
+    }),
+    // // NODE_ENV should be production so that modules do not perform certain development checks
+    new webpack.DefinePlugin({
+      //定义环境变量为开发环境
+      "process.env.NODE_ENV": JSON.stringify("development")
+    })
+  ]
+  // https://github.com/chentsulin/webpack-target-electron-renderer#how-this-module-works
+};
 
 module.exports = webpackConfigDev;
--- src/application/Main.js
+++ src/application/Main.js
@@ -1,29 +1,28 @@
 // In this file, we create a React component which contains components provided by Material-UI.
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import {IntlProvider,FormattedMessage} from 'react-intl';
-import configureStore from './store/configureStore'
-import intlConfig from './IntlConfig';
-import Routes from './routes';
-import {history} from './history';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+// import {IntlProvider,FormattedMessage} from 'react-intl';
+import configureStore from "./store/configureStore";
+// import intlConfig from "./IntlConfig";
+import Routes from "./routes";
+import { history } from "./history";
 // Needed for onTouchTap (http://stackoverflow.com/a/34015469/988941)
-const injectTapEventPlugin = require("react-tap-event-plugin");
-injectTapEventPlugin();
+// const injectTapEventPlugin = require("react-tap-event-plugin");
+// injectTapEventPlugin();
 const store = configureStore({});
 class Main extends Component {
-	constructor(properties, context) {
-		super(properties, context)
-	}
-	render() {
-		return (
-			<IntlProvider {...intlConfig}>
-				<Provider  store={store}>
-					<Routes history={history}>
-					</Routes>
-				</Provider>
-			</IntlProvider>
-		)
-	}
+  constructor(properties, context) {
+    super(properties, context);
+  }
+  render() {
+    return (
+      // <IntlProvider {...intlConfig}>
+      <Provider store={store}>
+        <Routes history={history}></Routes>
+      </Provider>
+      // </IntlProvider>
+    );
+  }
 }
 
 export default Main;
